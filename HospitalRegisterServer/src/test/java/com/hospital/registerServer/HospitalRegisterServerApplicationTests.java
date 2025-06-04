2c33dba0adac357b5898987d5af3370eafa6d642
20120082 <hieu123hcm@gmail.com>
2025-06-04 15:33:07 +0700

    Hospital Register Eureka Server

---
+++ HospitalRegisterServer/src/test/java/com/hospital/registerServer/HospitalRegisterServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.registerServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalRegisterServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
